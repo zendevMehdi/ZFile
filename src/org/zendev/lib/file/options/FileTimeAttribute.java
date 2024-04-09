7bf5799ecb46f295fb6242d2b346277682010499
Mehdi Lavasani <mehdilavasani79@gmail.com>
2024-04-09 16:30:14 +0330

    Fetch create, access, modify time and date of files and folders

---
+++ src/org/zendev/lib/file/options/FileTimeAttribute.java
@@ -0,0 +1,11 @@
+package org.zendev.lib.file.options;
+
+
+public enum FileTimeAttribute {
+    MODIFY_DATE,
+    MODIFY_TIME,
+    CREATE_DATE,
+    CREATE_TIME,
+    ACCESS_DATE,
+    ACCESS_TIME
+}
